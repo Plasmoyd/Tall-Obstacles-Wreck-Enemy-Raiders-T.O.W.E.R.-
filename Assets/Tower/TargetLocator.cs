1dfb1f546c30350eb34b3ba21dd3689ac62150b0
Ilija Starčević <ilijastarcevicgamedev@gmail.com>
2022-06-27 00:36:18 +0200

    Towers shoot at the closest target thats in range. If there are no targets in the scene, or they are out of range, they stop shooting. They change targets too quickly, maybe implement so that they shoot on a locked target until its out of range?

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -5,22 +5,66 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] float attackRange = 15f;
+    [SerializeField] ParticleSystem projectileSystem;
 
     Transform target;
 
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().GetComponent<Transform>();
-    }
 
     // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimAtTarget();
+    }
+
+    private void FindClosestTarget()
+    {
+        float minDistance = Mathf.Infinity;
+        Transform closestTarget = null;
+
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        foreach(Enemy enemy in enemies)
+        {
+            float currentDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(currentDistance < minDistance)
+            {
+                closestTarget = enemy.transform;
+                minDistance = currentDistance;
+            }
+
+            target = closestTarget;
+        }
     }
 
     private void AimAtTarget()
     {
+        if (target == null)
+        {
+            Attack(false);
+            return;
+        }
+
+        float currentDistance = Vector3.Distance(transform.position, target.transform.position);
+
         weapon.LookAt(target);
+
+
+        if (currentDistance <= attackRange)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    private void Attack(bool isActive)
+    {
+        var emissionModule = projectileSystem.emission;
+        emissionModule.enabled = isActive;
     }
 }
