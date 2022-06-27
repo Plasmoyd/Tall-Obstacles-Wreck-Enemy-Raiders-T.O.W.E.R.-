d6321272a02f6f0c220798841aca015248f82abb
Ilija Starčević <ilijastarcevicgamedev@gmail.com>
2022-06-27 03:24:21 +0200

    Created basic UI to display the gold balance

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -10,24 +11,34 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentGoldBalance;
     public int CurrentGoldBalance { get { return startingGoldBalance; } }
 
+    [SerializeField] TextMeshProUGUI text;
+
     private void Awake()
     {
         currentGoldBalance = startingGoldBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentGoldBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentGoldBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if(currentGoldBalance < 0)
         {
             ReloadScene();
         }
+    }
+
+    private void UpdateDisplay()
+    {
+        text.text = "Gold: " + currentGoldBalance;
     }
 
     private void ReloadScene()
